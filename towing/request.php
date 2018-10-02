98a83bf3e50269e27e9abf67ec883fed958f52d9
Mike Brown <michaelbrown.tsbod@gmail.com>
2018-10-02 19:18:10 +0000

    Updated

--- towing/request.php
+++ towing/request.php
@@ -129,60 +129,6 @@
                   <div class="input-group">
                      <span class="input-group-addon"><em class="glyphicon glyphicon-list"></em></span>
                      <input name="state" class="form-control" type="text" readonly value="Maryland">
-                     <!--<select name="state" class="form-control selectpicker" <>
-                        <option value=" " >Please select your state</option>
-                        <option value="Alabama">Alabama</option>
-                        <option value="Alaska">Alaska</option>
-                        <option value="Arizona">Arizona</option>
-                        <option value="Arkansas">Arkansas</option>
-                        <option  value="California">California</option>
-                        <option value="Colorado">Colorado</option>
-                        <option value="Connecticut">Connecticut</option>
-                        <option value="Delaware" >Delaware</option>
-                        <option value="DC" >District of Columbia</option>
-                        <option value="Florida"> Florida</option>
-                        <option value="Georgia" >Georgia</option>
-                        <option value="Hawaii">Hawaii</option>
-                        <option value="Idaho">Idaho</option>
-                        <option value="Illinois">Illinois</option>
-                        <option value="Indiana">Indiana</option>
-                        <option value="Iowa">Iowa</option>
-                        <option value="Kansas"> Kansas</option>
-                        <option value="Kentucky">Kentucky</option>
-                        <option value="Louisiana">Louisiana</option>
-                        <option value="Maine">Maine</option>
-                        <option value="Maryland">Maryland</option>
-                        <option value="Massachusetts">Massachusetts</option>
-                        <option value="Michigan">Michigan</option>
-                        <option value="Minnesota">Minnesota</option>
-                        <option value="Mississippi">Mississippi</option>
-                        <option value="Missouri">Missouri</option>
-                        <option value="Montona">Montana</option>
-                        <option value="Nebraska">Nebraska</option>
-                        <option value="Nevada">Nevada</option>
-                        <option value="New Hampshire">New Hampshire</option>
-                        <option value="New Jersey">New Jersey</option>
-                        <option value="New Mexico">New Mexico</option>
-                        <option value="New York">New York</option>
-                        <option value="North Carolina">North Carolina</option>
-                        <option value="North Doakota">North Dakota</option>
-                        <option value="Ohio">Ohio</option>
-                        <option value="Oklahoma">Oklahoma</option>
-                        <option value="Oregon">Oregon</option>
-                        <option value="Pennsylvania">Pennsylvania</option>
-                        <option value="Rhode Island">Rhode Island</option>
-                        <option value="South Carolina">South Carolina</option>
-                        <option value="South Dakota">South Dakota</option>
-                        <option value="Tennesse">Tennessee</option>
-                        <option value="Texas">Texas</option>
-                        <option value="Utah">Utah</option>
-                        <option value="Vermont">Vermont</option>
-                        <option value="Virginia">Virginia</option>
-                        <option value="Washington">Washington</option>
-                        <option value="West Virginia">West Virginia</option>
-                        <option value="Wisconsin">Wisconsin</option>
-                        <option value="Wyomin">Wyoming</option>
-                        </select>-->
                   </div>
                </div>
             </div>
@@ -343,22 +289,6 @@
                   </div>
                </div>
             </fieldset>
-            <!-- radio checks -->
-            <!-- <div class="form-group">
-               <label class="col-md-4 control-label">Do you have hosting?</label>
-               <div class="col-md-4">
-                   <div class="radio">
-                       <label>
-                           <input type="radio" name="hosting" value="yes" /> Yes
-                       </label>
-                   </div>
-                   <div class="radio">
-                       <label>
-                           <input type="radio" name="hosting" value="no" /> No
-                       </label>
-                   </div>
-               </div>
-               </div>-->
             <fieldset>
                <legend>Location Information</Legend>
                <div class="form-group">
