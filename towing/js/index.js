fccbbd960338295f639ca33a1740deea0d8b2b0b
Mike Brown <michaelbrown.tsbod@gmail.com>
2018-10-02 19:27:17 +0000

    updated

--- towing/js/index.js
+++ towing/js/index.js
@@ -191,9 +191,6 @@
             // Get the form instance
             var $form = $(e.target);
 
-            // Get the BootstrapValidator instance
-            var bv = $form.data('bootstrapValidator');
-
             // Use Ajax to submit form data
             $.post($form.attr('action'), $form.serialize(), function(result) {
                 console.log(result);
